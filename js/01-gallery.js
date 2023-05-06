979d331f28af037e44690bca11788988009e8458
OksanaPonom <ksyushono4ek@gmail.com>
2023-05-06 13:27:54 +0300

    update gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,7 +3,10 @@ import { galleryItems } from "./gallery-items.js";
 const gallery = document.querySelector(".gallery"); //посилання на елемент ul
 gallery.addEventListener("click", onClickImage); //додаємо слухача на ul
 
-function createGalleryMarkup(gallery) {   //функція створення розмітки
+let instance;
+
+function createGalleryMarkup(gallery) {
+  //функція створення розмітки
   return gallery
     .map(({ preview, original, description }) => {
       return `<li class="gallery__item">
@@ -21,36 +24,37 @@ function createGalleryMarkup(gallery) {   //функція створення р
 }
 gallery.insertAdjacentHTML("beforeend", createGalleryMarkup(galleryItems)); //додавання розмітки в елемент ul
 
-function onClickImage(evt) {  //функція, яка викликається при кліку на зображення
-  if (evt.target.nodeName !== "IMG") {    // якщо цільовий елемент не тег img, функція не спрацює
+function onClickImage(evt) {
+  //функція, яка викликається при кліку на зображення
+  if (evt.target.nodeName !== "IMG") {
+    // якщо цільовий елемент не тег img, функція не спрацює
     return;
   }
   evt.preventDefault();
-  
+
   let src = evt.target.getAttribute("src"); //присвоєння змінній src значення, яке дорівнює значенню атрибута src цільового елемента
   src = evt.target.getAttribute("data-source"); //заміна значення атрибута src на значення атрибута data-source у цыльового елемента
-  
-  const instance = basicLightbox.create(
+
+  instance = basicLightbox.create(
     `<img width="1400" height="900" src="${src}">`,
 
     {
-      onShow: () => {
-        addEventListener("keydown", onClosePressEsc);
-      }
-    },
-    {
-      onclose: () => {
-        removeEventListener("keydown", onClosePressEsc);
-      }
-    })
-  
-  function onClosePressEsc(event) {
-    if (event.code !== "Escape") {
-      return
+      onShow: (instance) => {
+        window.addEventListener("keydown", onClosePressEsc);
+      },
+
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onClosePressEsc);
+      },
     }
-    instance.close();
-  }
- 
-    instance.show();
+  );
+
+  instance.show();
 }
 
+function onClosePressEsc(event) {
+  if (event.code !== "Escape") {
+    return;
+  }
+  instance.close();
+}
